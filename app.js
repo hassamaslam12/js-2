2ed608adcf92daf81943a62bb93202e093ceb8bd
Muhammad Hassam <110339857+hassamaslam12@users.noreply.github.com>
2023-08-30 15:47:34 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,98 @@
+//////////////////////Chapter: 21////////////////////////////
+
+/*
+var userInput = prompt("Your Name?");
+var allLowerName = userInput.toLowerCase();
+console.log("lowered name : " + allLowerName)
+
+
+var x = "Hello This is Mike. How are you?";
+x = x.toLowerCase();
+
+var y = "too much of workload";
+y = y.toUpperCase();
+
+
+var z = "Hi! How are you doing Boy?";
+notDeclaredVariable = z.toLowerCase();
+console.log(notDeclaredVariable);
+
+var arrayOfStrings = ["Hello","Hi","Greetings"];
+nDV = arrayOfStrings[0].toLowerCase();
+console.log( nDV );
+
+var upperCasedMessage = "greetings! welcome to our website";
+alert(upperCasedMessage.toUpperCase());
+
+var cityName = "kaRacHi";
+cityName = cityName.toLowerCase();
+cityName = cityName.charAt(0).toUpperCase() + cityName.slice(1);
+console.log(cityName)
+*/
+
+
+
+
+///////////////////////// Chapter: 22-25 //////////////////////////////////
+
+/*
+// 1.
+var sameWords = "captain";
+console.log(sameWords.slice(1,3));
+
+// 2.
+var nunmberOfCharacters = sameWords.length;
+console.log(nunmberOfCharacters)
+
+//3.
+var animal = "elephant";
+seg = animal.slice(2,6);
+//4.
+var hello = "hello";
+var nOCinHello = hello.length;
+
+
+//5.
+var first = "hello buddy";
+second = first.slice(1,-3);
+console.log(second);
+
+//6.
+var text = "to be or not to be";
+console.log(text.indexOf('be'))
+/// Answer is 3 as indexOf gives the index of first letter.
+
+
+//7.
+console.log(text.lastIndexOf('be'))
+// Answer is 16 beacuse lasIndexOf gives the index of last time it is used in a string.
+
+
+//8. 
+var text = "bobby go";
+indx = text.lastIndexOf('go');
+
+//9.
+var text = "Hello how are you doing?"
+var indexNum = 9;
+if(text.length <= indexNum){
+console.log("index number is not part of the string")
+} 
+
+// 10.
+var text2 = "abcde";
+console.log("at index 2: " + text2.charAt(2))
+
+// 11.
+var text = "lkfejakljdadjklfaskdjaskldjasfhjfsda";
+cha = text.charAt(10);
+
+// 12.
+var str = "klacnklscn akjsnjkascn jkknacjksn";
+x = str.slice(-1);
+console.log("x: " + x);
+
+// 13.
+var input = "This is dummy text";
+x = input.charAt(5);
+*/
